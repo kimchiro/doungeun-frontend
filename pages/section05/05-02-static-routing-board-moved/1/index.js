c4b0c35165f62d4a4ec4ad5c5455f0df508cbd86
kimchiro <rlaehddhs12@gmail.com>
2024-04-24 21:50:17 +0900

    페이지 이동하기

---
+++ pages/section05/05-02-static-routing-board-moved/1/index.js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+export default function StaticRoutingMovedPage() {
+  const router = useRouter();
+  const onClickMove = () => {
+    router.push("/section05/05-02-static-routing-board");
+  };
+
+  return (
+    <div>
+      1번 게시글 이동이 완료되었습니다.
+      <button onClick={onClickMove}>메인으로 돌아가기</button>
+    </div>
+  );
+}
